4a9d7eba9b8a626f7277b5eaaacfd24c46dcaf08
Movie-Forever <112102089+Movie-Forever@users.noreply.github.com>
2024-04-22 00:17:30 -0400

    Update AirportGraph.h

--- AirportGraph.h
+++ AirportGraph.h
@@ -6,8 +6,10 @@
 class AirportGraph {
 public:
   void addFlight(Airport *source, Airport *destination, int distance, int cost);
-  Airport *getAirportByCode(std::string airportCode);
+  Airport *getAirportByCode(const std::string& airportCode);
   int get_airport_index(Airport* airport);
+  void PrimsAlgorithm();
+  void KruskalsAlgorithm();
   int get_shortest_path(Airport *src, Airport *dest);
   ~AirportGraph();
 
